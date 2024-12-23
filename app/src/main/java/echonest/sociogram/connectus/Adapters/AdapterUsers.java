e0b12277a5d1666eb37e1898d9fc421ef24d76ce
tashib11 <islam2007011@stud.kuet.ac.bd>
2024-12-23 11:23:51 +0600

    fixed the bugs

--- app/src/main/java/echonest/sociogram/connectus/Adapters/AdapterUsers.java
+++ app/src/main/java/echonest/sociogram/connectus/Adapters/AdapterUsers.java
@@ -17,6 +17,7 @@ import echonest.sociogram.connectus.Models.ModelUser;
 import com.example.connectus.R;
 import com.squareup.picasso.Picasso;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class AdapterUsers extends  RecyclerView.Adapter<AdapterUsers.MyHolder> {
@@ -30,57 +31,53 @@ public class AdapterUsers extends  RecyclerView.Adapter<AdapterUsers.MyHolder> {
     public AdapterUsers(Context context, List<ModelUser> userList) {
         this.context = context;
         this.userList = userList;
+
     }
 
     @NonNull
     @Override
     public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(context).inflate(R.layout.row_users, parent,false);
+        View view= LayoutInflater.from(context).inflate(R.layout.row_peoplelist, parent,false);
         return new MyHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyHolder holder, int position) {
 // get data
-        String UID=userList.get(position).getUserId();
-        String userImage=userList.get(position).getProfilePhoto();
-        String userName=userList.get(position).getName();
-//        String email=userList.get(position).getEmail();
+        ModelUser user = userList.get(position);
+        String hisUid = user.getUserId();
+        String userImage = user.getProfilePhoto();
+        String userName = user.getName();
 
         //set data
-        holder.mNameTv.setText(userName);
+        holder.nameTv.setText(userName);
+//        String email=userList.get(position).getEmail();
 
-        if (userImage != null && !userImage.trim().isEmpty()) {
+
+
+        try {
             Glide.with(context)
                     .load(userImage)
                     .placeholder(R.drawable.avatar)
-                    .into(holder.mAvatarIv);
-//            Picasso.get().load(userImage).placeholder(R.drawable.avatar).into(holder.mAvatarIv);
-        } else {
+                    .into(holder.profileIv);
+        } catch (Exception e) {
             Glide.with(context)
                     .load(R.drawable.avatar)
-                    .into(holder.mAvatarIv);
-//            Picasso.get().load(R.drawable.avatar).into(holder.mAvatarIv);
-            // Load a default image or handle the case when userImage is null or empty
+                    .into(holder.profileIv);
         }
-        try{
-            Picasso.get().load(userImage).placeholder(R.drawable.avatar).into(holder.mAvatarIv);
-        }catch (Exception e){
-            Picasso.get().load(R.drawable.avatar).into(holder.mAvatarIv);
+
+
+        String onlineStatus = userList.get(position).getOnlineStatus();
+        if(onlineStatus != null && onlineStatus.equals("online")){
+            holder.onlineStatusIv.setImageResource(R.drawable.circle_online);
+        }else{
+            holder.onlineStatusIv.setImageResource(R.drawable.circle_offline);
         }
         // handle item click
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            /*
-            Click user form user list to start chatting
-            start activity by putting UID of receiover
-            we will use that uid to identify the user we are gonna chat
-             */
-                Intent intent= new Intent(context, ChatDetailActivity.class);
-                intent.putExtra("hisUid",UID);
-                context.startActivity(intent);
-            }
+        holder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(context, ChatDetailActivity.class);
+            intent.putExtra("hisUid", hisUid);
+            context.startActivity(intent);
         });
     }
 
@@ -90,15 +87,15 @@ public class AdapterUsers extends  RecyclerView.Adapter<AdapterUsers.MyHolder> {
         return userList.size();
     }
 
-    class MyHolder extends RecyclerView.ViewHolder{
+    static class MyHolder extends RecyclerView.ViewHolder {
+        ImageView profileIv, onlineStatusIv;
+        TextView nameTv ;
 
-        ImageView mAvatarIv;
-        TextView mNameTv,mEmailTv;
-        public  MyHolder(View itemview){
-            super(itemview);
-            mAvatarIv=itemview.findViewById(R.id.avatarIv);
-            mNameTv=itemview.findViewById(R.id.nameTv);
-//            mAvatarIv=itemview.findViewById(R.id.avatarIv);
+        MyHolder(@NonNull View itemView) {
+            super(itemView);
+            profileIv = itemView.findViewById(R.id.profileIv);
+            onlineStatusIv = itemView.findViewById(R.id.onlineStatusIv);
+            nameTv = itemView.findViewById(R.id.nameTv);
 
         }
     }
