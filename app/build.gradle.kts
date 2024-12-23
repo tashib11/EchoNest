bac4f2edd1782548d64c770b57e148c1cba13878
tashib11 <islam2007011@stud.kuet.ac.bd>
2024-12-23 22:08:39 +0600

    to make pic blur at sending

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,7 @@ implementation("com.airbnb.android:lottie:3.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("jp.wasabeef:glide-transformations:4.3.0")
+
 }
