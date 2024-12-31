8b6602c11a87f93a36df11fe008b2146d4b923b8
tashib11 <islam2007011@stud.kuet.ac.bd>
2025-01-01 01:04:31 +0600

    online status circle green light fixed

--- app/src/main/java/echonest/sociogram/connectus/MainActivity.java
+++ app/src/main/java/echonest/sociogram/connectus/MainActivity.java
@@ -25,7 +25,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
-    private static boolean isAppInForeground = false;
+    private boolean isAppClosing = false; // Track if the app is being closed
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     BottomNavigationView bottomNavigationView;
@@ -34,138 +34,128 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseUser currentUser;
     DatabaseReference userRef;
-    GoogleSignInClient mGoogleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // for  status bar color
+
+        // Set the status bar color
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        ((Window) window).setStatusBarColor(this.getResources().getColor(R.color.black));
-//        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.black)));
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        window.setStatusBarColor(this.getResources().getColor(R.color.black));
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        auth=FirebaseAuth.getInstance();
+
+        auth = FirebaseAuth.getInstance();
         currentUser = auth.getCurrentUser();
 
         if (currentUser != null) {
             String userId = currentUser.getUid();
             userRef = FirebaseDatabase.getInstance().getReference("Users").child(userId);
 
-            // Set the current timestamp when the app is terminated
-            userRef.child("onlineStatus").onDisconnect().setValue(String.valueOf(System.currentTimeMillis()));
-
-            // Initially set the user to "online"
-            userRef.child("onlineStatus").setValue("online");
+            // Set the user as online when the app is running
+            userRef.child("onlineStatus").setValue("online")
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            // Configure onDisconnect to set status to last active time
+                            userRef.child("onlineStatus").onDisconnect().setValue(String.valueOf(System.currentTimeMillis()));
+                        }
+                    });
         }
 
-        drawerLayout=findViewById(R.id.drawerLayout);
-        navigationView=findViewById((R.id.nav_drawer));
-        bottomNavigationView=findViewById(R.id.bottom_navigation);
-        toolbar=findViewById((R.id.toolbar));
+        // Navigation code remains unchanged
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav_drawer);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        toolbar = findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
-        ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.OpenDrawer,R.string.CloseDrawer);
-        //activity ,drawer,toolbar and 2flex-open-close reference push kora lagbe in ActionBarDrawerToggle()
-        //flex ar value int dite hobe but int ta string hishebe thakbe .so go vlues->string a jao
-        drawerLayout.addDrawerListener(toggle);//for sliding , mane shob vabei drawer use kora jabe
-        toggle.syncState();// open hole bolbe ji open vai , close hole state ta hobe close
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.OpenDrawer, R.string.CloseDrawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
         getSupportActionBar().setTitle("Chats");
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id= item.getItemId();
-                if (id == R.id.logout) {
-                    if (currentUser != null) {
-                        String userId = currentUser.getUid();
-                        userRef = FirebaseDatabase.getInstance().getReference("Users").child(userId);
 
-                        // Set the current timestamp before logging out
-                        userRef.child("onlineStatus").setValue(String.valueOf(System.currentTimeMillis()))
-                                .addOnCompleteListener(task -> {
-                                    if (task.isSuccessful()) {
-                                        // Sign out FirebaseAuth
-                                        auth.signOut();
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.logout) {
+                if (currentUser != null) {
+                    userRef.child("onlineStatus").setValue(String.valueOf(System.currentTimeMillis()))
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    auth.signOut();
+                                    Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Failed to update status. Please try again.", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            } else if (id == R.id.settings) {
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            } else if (id == R.id.deleteAccount) {
+                startActivity(new Intent(MainActivity.this, deleteAccountActivity.class));
+            } else if (id == R.id.news) {
+                startActivity(new Intent(MainActivity.this, newsActivity.class));
+            }
+            return true;
+        });
 
-                                        // Redirect to SignInActivity
-                                        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
-                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                        startActivity(intent);
-                                        finish();
-                                    } else {
-                                        Toast.makeText(MainActivity.this, "Failed to update status. Please try again.", Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-                    }
-                }
-
-                else if(id==R.id.settings){
-                    Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                    startActivity(intent);
-                }
-                else if(id==R.id.deleteAccount){
-                    Intent intent = new Intent(MainActivity.this, deleteAccountActivity.class);
-                    startActivity(intent);
-                }       else if(id==R.id.news){
-                    Intent intent = new Intent(MainActivity.this, newsActivity.class);
-                    startActivity(intent);
-                }
+        binding.viewPager.setAdapter(new FragmentsAdapter(getSupportFragmentManager()));
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.bchat) {
+                binding.viewPager.setCurrentItem(0);
+                getSupportActionBar().setTitle("Chats");
+                return true;
+            } else if (id == R.id.bpeople) {
+                binding.viewPager.setCurrentItem(1);
+                getSupportActionBar().setTitle("People");
                 return true;
             }
-        });
-        binding.viewPager.setAdapter(new FragmentsAdapter(getSupportFragmentManager()));
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id= item.getItemId();
-                if(id==R.id.bchat){
-                    binding.viewPager.setCurrentItem(0);
-                    getSupportActionBar().setTitle("Chats");
-                    return  true;
-                }
-                else   if(id==R.id.bpeople){
-                    binding.viewPager.setCurrentItem(1);
-//                    getActionBar().setTitle("Chats");
-                    getSupportActionBar().setTitle("People");
-                    return  true;
-                }
-                return false;
-            }
+            return false;
         });
     }
-    public void onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-            drawerLayout.closeDrawer(GravityCompat.START);
-            // if u pressed back button then drawer will be off
-        }else {
-//          super.onBackPressed();// if already drwer is off then by pressing backbutton ,app exit
-            finishAffinity();
-            finish();
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isAppClosing = false; // Reset app-closing flag
+        if (currentUser != null && userRef != null) {
+            // Keep the user online when the app is running
+            userRef.child("onlineStatus").setValue("online");
         }
     }
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu, menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-@Override
-protected void onResume() {
-    super.onResume();
-    if (currentUser != null && userRef != null) {
-        userRef.child("onlineStatus").setValue("online");
-    }
-    isAppInForeground = true;
-}
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isAppClosing && currentUser != null && userRef != null) {
+            // Set to timestamp when the app is about to go in the background
+            userRef.child("onlineStatus").setValue(String.valueOf(System.currentTimeMillis()));
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (isAppClosing && currentUser != null && userRef != null) {
+            // Set to timestamp when the app is destroyed (app is closed)
+            userRef.child("onlineStatus").setValue(String.valueOf(System.currentTimeMillis()));
+        }
+    }
 
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        if (currentUser != null && userRef != null) {
-            userRef.child("onlineStatus").setValue("online");
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            isAppClosing = true; // Mark app as closing
+            finishAffinity();
+            finish();
         }
-    }
-
-
-}
+    }}
